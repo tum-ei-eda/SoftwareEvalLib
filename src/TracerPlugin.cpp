b706d6b019031da7d4206b6c29c867e059be4c36
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-01-21 15:11:06 +0100

    src/TracerPlugin.cpp: fix

--- src/TracerPlugin.cpp
+++ src/TracerPlugin.cpp
@@ -119,11 +119,11 @@ void TracerPlugin::addMonitor(Monitor* monitor_){
     std::cout << "Adding Monitor: " << monitor_->name << std::endl;
 }
 
-etiss::int64 TracerPlugin::getEstimatedCycleCount()
-{
-  return 0;
-
-}
+// etiss::int64 TracerPlugin::getEstimatedCycleCount()
+// {
+//   return 0;
+//
+// }
 
 } // namespace TracerPlugin
 } // namespace plugin
